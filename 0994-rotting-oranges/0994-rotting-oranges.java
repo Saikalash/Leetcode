f2375c03cae7baae5f801e485dc0596afcbbab74
Sanji <kalashsai63@gmail.com>
2026-08-23 02:33:08 +0530

    Time: 2 ms (89.58%), Space: 43.9 MB (85.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,61 +1,53 @@
 class Solution {
-    int min=0;
     public int orangesRotting(int[][] a) {
-        int n =a.length;
-        int m =a[0].length;
-      //  int min=0;
-      
-      int fresh=0;
-      Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<n;i++)
+       int n =a.length;
+       int m = a[0].length;
+       int fresh=0;
+       Queue<int[]> q = new LinkedList<>();
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<m;j++)
+            if(a[i][j]==1)
             {
-                if(a[i][j]==2)
+                fresh++;
+            }
+            if(a[i][j]==2)
+            {
+                q.offer(new int[]{i,j});
+            }
+        }
+       }
+       if(fresh==0)
+       return 0;
+       int min=0;
+       int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+       while(!q.isEmpty())
+       {
+        int p =q.size();
+        while(p-->0)
+        {
+            int curr[] = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            for(int dir[]:directions)
+            {
+                int x1 = x+dir[0];
+                int y1 = y+dir[1];
+                if(x1>=0 && x1<n && y1>=0 && y1<m && a[x1][y1]==1)
                 {
-                    q.offer(new int[]{i,j});
-                }
-                if(a[i][j]==1)
-                {
-                    fresh++;
+                    fresh--;
+                    a[x1][y1]=2;
+                    q.offer(new int[]{x1,y1});
                 }
             }
         }
-        if(fresh==0)
-        return 0;
-        int min=0;
-        int directions[][] = {{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.isEmpty())
-        {
-            int p =q.size();
-            while(p-->0)
-            {
-                int curr[] = q.poll();
-                int x = curr[0];
-                int y = curr[1];
-                for(int[] dir:directions)
-                {
-                    int x1 = x+dir[0];
-                    int x2 = y+dir[1];
-                    if(x1>=0 && x2>=0 && x1<n && x2<m && a[x1][x2]==1)
-                    {
-                        a[x1][x2]=2;
-                        fresh--;
-                        q.offer(new int[]{x1,x2});
-                    }
-                }
-                
-
-            }
-            min++;
-        }
-        if(fresh!=0)
-        {
-            return -1;
-        }
-        return min-1;
-
-
-        //return min;
+        min++;
+       }
+       if(fresh!=0)
+       {
+        return -1;
+       }
+       return min-1;
     }
 }
