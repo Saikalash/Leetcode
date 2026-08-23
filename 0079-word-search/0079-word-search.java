2838f99136640e7a645d67fda5ec0a1c0bd2c521
Sanji <kalashsai63@gmail.com>
2026-08-24 00:44:32 +0530

    Time: 259 ms (8.83%), Space: 46.8 MB (7.87%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean exist(char[][] a, String x) {
+        int n =a.length;
+        int m = a[0].length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++){
+                if(a[i][j]==x.charAt(0) && solve(i,j,x,0,a))
+                {
+                   return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean solve(int i,int j,String x,int idx,char a[][])
+    {
+        int n=a.length;
+        int m =a[0].length;
+        if(i<0 || i>=n || j<0 || j>=m || a[i][j] =='$'|| a[i][j]!=x.charAt(idx))
+        {
+            return false;
+        }
+        if(idx == x.length()-1)
+        {return true;
+        }
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        char temp = a[i][j];
+        a[i][j]='$';
+        for(int[] dir:directions)
+        {
+            int p = i+dir[0];
+            int q = j+dir[1];
+
+            if(solve(p,q,x,idx+1,a))
+            return true;
+        }
+        a[i][j]=temp;
+
+        
+    return false;
+
+    }
+}
